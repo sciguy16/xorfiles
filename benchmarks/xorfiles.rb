e7d0b48022a98c17811c9c50213a295a5b87d529
David Young <david@thedavidyoung.co.uk>
2020-02-28 22:23:07 +0000

    Add explanation of zip optimisation

--- benchmarks/xorfiles.rb
+++ benchmarks/xorfiles.rb
@@ -17,6 +17,15 @@ f2 = IO.new(f2desc, 'r')
 
 BUFSIZE = 2048
 
+# Zip will attempt to assign an element of its argument array to each
+# element of the target array, and if there aren't enough the remainder
+# will be filled in with nil. This is bad because nil can't be coerced
+# into Integer, so we have to first read from the argument array and
+# if fewer than BUFSIZE are read in then we only read that much into the
+# target array.
+#
+# This is measurably faster than running a select() on the zip to pull
+# out non-nil entries (intuitively).
 begin
   while true do
     buf2 = f2.readpartial(BUFSIZE).bytes
