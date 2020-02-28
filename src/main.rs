fea066354fcfe0c357ef4bce207ba1f7148bceca
David Young <david@thedavidyoung.co.uk>
2020-02-28 17:59:33 +0000

    Comment out unimplemented bits and debug prints

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use structopt::StructOpt;
-use std::io::stdin;
-//use std::io::Stdin;
-//use std::io::BufReader;
-//use std::io::BufRead;
+//use std::io::stdin;
 use std::io;
 use std::fs::File;
 use std::path::PathBuf;
@@ -21,47 +18,59 @@ struct Opts {
     #[structopt(parse(from_os_str))]
     file2: Option<PathBuf>,
 
-    /// Optionally loop the shorter file around until the longer file
-    /// runs out
+    // /// Optionally loop the shorter file around until the longer file
+    // /// runs out
     // "loop" is a reserved keyword so we can't name the variable that :(
-    #[structopt(short = "l", long = "loop")]
-    loop_shorter: bool,
+    //#[structopt(short = "l", long = "loop")]
+    //loop_shorter: bool,
+
+    // /// Optional limit on the number of bytes to read
+    //#[structopt(short = "c", long = "count", default_value = "0")]
+    //count: usize,
 }
 
 fn main() -> io::Result<()> {
-    println!("Hello, world!");
-
     let opts = Opts::from_args();
-    println!("{:?}", opts);
+    //if opts.loop_shorter {
+    //    unimplemented!();
+    //}
 
     let f1 = File::open(opts.file1)?;
+    //let f1meta = f1.metadata()?;
     let mut buf1 = f1.bytes();
 
-    let mut buf2 = if let Some(file2) = opts.file2 {
-        Some(File::open(file2)?.bytes())
+    let f2;
+    let buf2 = if let Some(file2) = opts.file2 {
+        f2 = File::open(file2)?;
+        Some(f2.bytes())
     } else {
         None
     };
 
     let mut buf2 = buf2.unwrap();
 
-    let mut writer = io::stdout();
-
     loop {
         let b1 = buf1.next();
         let b2 = buf2.next();
+        //eprintln!("Loop: ({:?}, {:?})", b1, b2);
         match (b1, b2) {
+            (Some(Ok(a)), Some(Ok(b))) => {
+                io::stdout().write(&[a ^ b])?;
+                //eprint!("=");
+            },
             (None, None) => {
-                eprintln!("The end :(");
+                //eprintln!("The end :(");
                 break;
             },
-            (Some(Ok(a)), Some(Ok(b))) => {
-                writer.write(&[a ^ b]);
+            _x => {
+                //eprintln!("End: {:?}", x);
+                break;
             },
-            _ => break,
         }
 
     }
+
+    io::stdout().flush()?;
 
     Ok(())
 }
