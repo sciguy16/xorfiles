c7eebcfd9f8ffb5830c6c86ca1a9afa128cbd89b
David Young <david@thedavidyoung.co.uk>
2020-02-28 16:47:05 +0000

    Implement basic XOR of two files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use structopt::StructOpt;
+use std::io::stdin;
+//use std::io::Stdin;
+//use std::io::BufReader;
+//use std::io::BufRead;
+use std::io;
+use std::fs::File;
 use std::path::PathBuf;
+use std::io::prelude::*;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "xorfiles", about = "XOR two files together")]
@@ -21,9 +28,40 @@ struct Opts {
     loop_shorter: bool,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     println!("Hello, world!");
 
-    let opt = Opts::from_args();
-    println!("{:?}", opt);
+    let opts = Opts::from_args();
+    println!("{:?}", opts);
+
+    let f1 = File::open(opts.file1)?;
+    let mut buf1 = f1.bytes();
+
+    let mut buf2 = if let Some(file2) = opts.file2 {
+        Some(File::open(file2)?.bytes())
+    } else {
+        None
+    };
+
+    let mut buf2 = buf2.unwrap();
+
+    let mut writer = io::stdout();
+
+    loop {
+        let b1 = buf1.next();
+        let b2 = buf2.next();
+        match (b1, b2) {
+            (None, None) => {
+                eprintln!("The end :(");
+                break;
+            },
+            (Some(Ok(a)), Some(Ok(b))) => {
+                writer.write(&[a ^ b]);
+            },
+            _ => break,
+        }
+
+    }
+
+    Ok(())
 }
